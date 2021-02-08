6b25e2c4c93aa2f8891d71b6985627846b4d5977
gquintin <guillaume.quintin@numscale.com>
2021-02-08 17:17:43 +0100

    nsconfig: NVCC does not pass defines to host compiler

--- nsconfig/shell.cpp
+++ nsconfig/shell.cpp
@@ -1270,6 +1270,7 @@ nvcc(compiler::infos_t const &ci, std::vector<parser::token_t> const &tokens,
         if (arg.size() == 2) {
           die("no macro name given here", tokens[i].cursor);
         }
+        host_tokens.push_back(arg);
         ret.push_back(arg);
       } else {
         host_tokens.push_back(tokens[i]);
